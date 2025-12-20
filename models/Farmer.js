a2dde63b15cb261819007f63c2604086d3472351
chandakakarthik19-ship-it <chandakakarthik19@gmail.com>
2025-12-20 14:55:44 +0530

    Update Farmer.js

--- models/Farmer.js
+++ models/Farmer.js
@@ -1,22 +1,55 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
-const farmerSchema = new mongoose.Schema({
-  name: String,
-  phone: { type: String, unique: true },
-  password: String
+const PaymentSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  workId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Work'
+  }
 });
 
-// Hash password before save
-farmerSchema.pre('save', async function(next){
+const FarmerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profileImage: {
+    type: String
+  },
+  payments: [PaymentSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+/* HASH PASSWORD */
+FarmerSchema.pre('save', async function(next){
   if(!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare password
-farmerSchema.methods.comparePassword = function(password){
+/* COMPARE PASSWORD */
+FarmerSchema.methods.comparePassword = function(password){
   return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('Farmer', farmerSchema);
+module.exports = mongoose.model('Farmer', FarmerSchema);
